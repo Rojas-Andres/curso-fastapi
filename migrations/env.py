946d4ca93e0cd62316164e7ccc29432be3188d7b
Rojas-Andres <anferoja@bancolombia.com.co>
2022-06-14 00:23:38 -0500

    añadiendo informacion al readme

--- migrations/env.py
+++ migrations/env.py
@@ -6,27 +6,16 @@ from sqlalchemy import pool
 from alembic import context
 
 from core.config import settings
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+
 config = context.config
 config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-# target_metadata = None
 from app.db.models import Base
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline():
--- readme.md
+++ readme.md
@@ -5,3 +5,25 @@
             uvicorn.run("main:app",port=8000)
         
         Y luego correr main.py
+
+# Realizar Migraciones
+
+alembic init migrations
+
+# configurar alembic
+    - abriri el alembic.ini y modificar la linea de sqlalchemy.url y dejar vacio
+# Abrir el env.py de la carpeta migrations
+    Copiar esto antes de la funcion run_migrations_offline
+    from core.config import settings
+
+    config = context.config
+    config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
+
+    if config.config_file_name is not None:
+        fileConfig(config.config_file_name)
+
+    from app.db.models import Base
+    target_metadata = Base.metadata
+# Por ultimo ejecutar
+alembic revision --autogenerate -m "crear modelos"
+alembic upgrade heads
