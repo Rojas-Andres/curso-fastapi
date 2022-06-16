3ae6027879a9e142c0f1cd6a59d39cef7657518e
Rojas-Andres <anferoja@bancolombia.com.co>
2022-06-16 14:15:00 -0500

    Añadiendo Procfile

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ app.include_router(user.router)
 
 app.include_router(auth.router)
 
-if __name__=="__main__":
-    uvicorn.run("main:app",port=8000,reload=True)
+# if __name__=="__main__":
+#     uvicorn.run("main:app",port=8000,reload=True)
